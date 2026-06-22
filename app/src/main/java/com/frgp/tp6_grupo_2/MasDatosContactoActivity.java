82765dc374f193c787fe2ab15ea0481e6a4b9593
SebastianDte <sebateunmate@outlook.com>
2026-06-22 16:58:41 -0300

    TP 6.8 Switch y Evento Guardar

--- app/src/main/java/com/frgp/tp6_grupo_2/MasDatosContactoActivity.java
+++ app/src/main/java/com/frgp/tp6_grupo_2/MasDatosContactoActivity.java
@@ -1,6 +1,8 @@
 package com.frgp.tp6_grupo_2;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.RadioGroup;
 import android.widget.TextView;
@@ -8,6 +10,7 @@ import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -20,6 +23,9 @@ public class MasDatosContactoActivity extends AppCompatActivity {
     private CheckBox cbArte;
     private CheckBox cbTecnologia;
     private RadioGroup rgNivelEstudios;
+
+    private SwitchCompat swInformacion;
+    private Button btnGuardar;
 
 
     @Override
@@ -36,6 +42,7 @@ public class MasDatosContactoActivity extends AppCompatActivity {
 
         TextView tvDatosRecibidos = findViewById(R.id.tvDatosRecibidos);
 
+        // Datos que vienen de la primera pantalla
         String nombre = getIntent().getStringExtra("nombre");
         String apellido = getIntent().getStringExtra("apellido");
         String direccion = getIntent().getStringExtra("direccion");
@@ -43,22 +50,39 @@ public class MasDatosContactoActivity extends AppCompatActivity {
         String email = getIntent().getStringExtra("email");
         String fecha = getIntent().getStringExtra("fechaNacimiento");
 
-        // comenté para poder probar bien la vista. Falta quitar en el XML
-//        tvDatosRecibidos.setText(
-//                "Mas Datos Contacton\n\n" +
-//                        "Nombre: " + nombre + "\n" +
-//                        "Apellido: " + apellido + "\n" +
-//                        "Dirección: " + direccion + "\n" +
-//                        "Teléfono: " + telefono + "\n" +
-//                        "Email: " + email + "\n" +
-//                        "Fecha: " + fecha
-//        );
         rgNivelEstudios = findViewById(R.id.rgNivelEstudios);
 
         cbDeporte = findViewById(R.id.cbDeporte);
         cbMusica = findViewById(R.id.cbMusica);
         cbArte = findViewById(R.id.cbArte);
         cbTecnologia = findViewById(R.id.cbTecnologia);
+
+        swInformacion = findViewById(R.id.swInformacion);
+        btnGuardar = findViewById(R.id.btnGuardar);
+
+        // EVENTO DEL BOTÓN GUARDAR ===
+        btnGuardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                if (!validarNivelEstudios() || !validarIntereses()) {
+                    return;
+                }
+                String nombreFinal = nombre;
+                String apellidoFinal = apellido;
+                String direccionFinal = direccion;
+                String telefonoFinal = telefono;
+                String emailFinal = email;
+                String fechaFinal = fecha;
+
+                boolean aceptaInformacion = swInformacion.isChecked();
+
+                // =========================================================
+                // TODO: ACÁ VA EL TICKET TP 6.10 (SQLite INSERT)
+                // =========================================================
+            }
+        });
     }
 
     private boolean validarNivelEstudios() {
